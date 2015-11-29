a47471d9a809a1c8e4eb24a9d8197f319af98dad
anil <aaltinay@uci.edu>
2015-11-28 19:45:21 -0800

    filter fixed

--- qe/qe.h
+++ qe/qe.h
@@ -265,14 +265,10 @@ class BNLJoin : public Iterator {
         unsigned numPages;
         bool hasMore;
         unsigned totalBufferSize;
-<<<<<<< HEAD
         //char *buffer;
         vector<void *> bufferV;
         std::unordered_map<string, vector<unsigned> > tuplesMap;
-=======
-        char *buffer;
-        //std::unordered_map<string, vector<void *> > tuplesMap;
->>>>>>> b42690a25d6ed75af9940861dec349743b6ccfbc
+
 
 };
 
